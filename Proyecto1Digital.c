8368ff5254da161ae65eea9f9360fc9ebbb9273f
Diego <shorento123@gmail.com>
2021-02-20 19:28:37 -0600

    Configuramos el ADC

---
+++ Proyecto1Digital.c
@@ -0,0 +1,17 @@
+/*
+ * File:   Proyecto1Digital.c
+ * Author: diegomazariegos
+ *
+ * Created on February 20, 2021, 2:55 PM
+ */
+
+
+#include <xc.h>
+#include <stdio.h>
+#include "config.h"
+#define  _XTAL_FREQ 8000000;
+
+
+void main(void) {
+    return;
+}
